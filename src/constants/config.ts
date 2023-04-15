039aad7f87682359c97b3359bb86c53d92adc2a0
realsnoopso <real@snoop.so>
2023-04-15 15:38:33 +0900

    Add api keys

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,3 +2,5 @@ import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
 export const AMPLITUDE_KEY = publicRuntimeConfig.AMPLITUDE_KEY;
+export const OPENAI_API_KEY = publicRuntimeConfig.OPENAI_API_KEY;
+export const FLIPSIDE_API_KEY = publicRuntimeConfig.FLIPSIDE_API_KEY;
